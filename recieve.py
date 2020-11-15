dc247d531fc8ad7302ba59f54b67170e58b73f35
zfima <zzfima@gmail.com>
2020-11-15 11:04:04 +0200

    rfct

--- recieve.py
+++ recieve.py
@@ -1,30 +1,16 @@
 import pika
-import sys
-import os
 
 
 def callback(ch, method, properties, body):
     print(" [x] Received %r" % body)
 
 
-def main():
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
-    channel = connection.channel()
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
 
-    channel.queue_declare(queue='hello')
+channel.queue_declare(queue='hello')
 
-    channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
+channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
 
-    print(' [*] Waiting for messages. To exit press CTRL+C')
-    channel.start_consuming()
-
-
-if __name__ == '__main__':
-    try:
-        main()
-    except KeyboardInterrupt:
-        print('Interrupted')
-        try:
-            sys.exit(0)
-        except SystemExit:
-            os._exit(0)
+print(' [*] Waiting for messages. To exit press CTRL+C')
+channel.start_consuming()
--- send.py
+++ send.py
@@ -9,6 +9,7 @@ channel.queue_declare(queue='hello')
 
 while True:
     time.sleep(2)
-    channel.basic_publish(exchange='', routing_key='hello', body='Hello World!')
+    body_to_send = 'Hello World!' + ' ' + str(time.time())
+    channel.basic_publish(exchange='', routing_key='hello', body=body_to_send)
 print(" [x] Sent 'Hello World!'")
 connection.close()
